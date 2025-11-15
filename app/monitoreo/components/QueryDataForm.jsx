935183687ca6c78e976e519673e3b7115230723a
davidruizdiaz <adavidruizdiaz@gmail.com>
2025-11-15 14:36:05 +0000

    up: formulario de consulta

--- app/monitoreo/components/QueryDataForm.jsx
+++ app/monitoreo/components/QueryDataForm.jsx
@@ -4,14 +4,14 @@ import SelectRios from "./SelectRios";
 
 const QueryDataForm = () => {
   return (
-    <div className="w-[30%] mb-2 border border-gray-300 p-2 rounded shadow-lg">
+    <div className="w-[95%] md:w-[30%] mb-2 border border-gray-300 p-2 rounded shadow-lg">
       <h6 className="pb-1 text-center font-medium">Selección de parámetros</h6>
-      <from className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2">
         <SelectRios />
         <SelectParametros />
         <DateInput />
         <button className="btn btn-sm btn-soft btn-primary">Consultar</button>
-      </from>
+      </form>
     </div>
   );
 }
